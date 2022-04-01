ddb4f2e898041534c09e4ae239be0e07e9540829
Hammami <ahmed.hammami.1@esprit.tn>
2022-04-01 16:29:25 +0200

    db fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.HEROKU_POSTGRESQL_BROWN_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
