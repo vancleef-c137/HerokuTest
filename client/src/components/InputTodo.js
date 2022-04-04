f192bc187e5bfcde508d8a30a3f3f69da0f8d7c6
Hammami <ahmed.hammami.1@esprit.tn>
2022-04-04 10:50:49 +0200

    tojrab

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -7,7 +7,7 @@ const InputTodo = () => {
     e.preventDefault();
     try {
       const body = { description };
-      const response = await fetch("http://localhost:3000/todos", {
+      const response = await fetch("/todos", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(body)
@@ -21,7 +21,7 @@ const InputTodo = () => {
 
   return (
     <Fragment>
-      <h1 className="text-center mt-5">Pern Todo List</h1>
+      <h1 className="text-center mt-5">EL TO DO </h1>
       <form className="d-flex mt-5" onSubmit={onSubmitForm}>
         <input
           type="text"
--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.post("/todos", async(req,res) =>{
     try {
         const { description } = req.body;
         const newTodo = await pool.query(
-            "INSERT INTO salesforce.todos__c(description__c, externalid__c) VALUES ($1,'7848')",
+            "INSERT INTO salesforce.todos__c(description__c, externalid__c) VALUES ($1,'79848')",
             //"INSERT INTO todo (description) VALUES($1) RETURNING *",
             //"INSERT INTO salesforce.todos__c(description__c, externalid__c) VALUES($1, '3') RETURNING *"
 
@@ -91,7 +91,7 @@ app.get("/todos", async(req,res)=>{
 app.get("/todos/:id", async(req,res)=>{
     try {
         const {id} = req.params;
-        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+        const todo = await pool.query("SELECT * FROM salesforce.todos__c WHERE todo_id = $1", [id]);
         res.json(todo.rows[0]);
     } catch (error) {
         console.error(error.message)
@@ -107,7 +107,7 @@ app.put("/todos/:id", async(req,res)=>{
         const {id} =req.params;
         const {description} = req.body;
         const updateTodo = await pool.query(
-            "UPDATE todo SET description = $1 WHERE todo_id = $2",
+            "UPDATE salesforce.todos__c SET description = $1 WHERE todo_id = $2",
             [description, id]
             );
             res.json("Todo was updated!");
@@ -124,7 +124,7 @@ app.put("/todos/:id", async(req,res)=>{
 app.delete("/todos/:id", async(req,res)=>{
     try {
         const {id} = req.params;
-        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id =$1", [id]
+        const deleteTodo = await pool.query("DELETE FROM salesforce.todos__c WHERE todo_id =$1", [id]
 );
 res.json("Todo was Deleted!");
     } catch (error) {
